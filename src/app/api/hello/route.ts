a70831e9f90546072324113aff932f3286e2bf9d
teplostanski <teplostanski@yandex.ru>
2023-11-11 18:41:04 +0300

    test ssr 5

--- src/app/api/hello/route.ts
+++ src/app/api/hello/route.ts
@@ -1,6 +1,5 @@
 import { NextResponse } from 'next/server';
 
 export async function GET(request: Request) {
-  //const d = { heello: 'lkiuytrrftghjkl' }
   return NextResponse.json({ hello: 'qwer' })
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -9,6 +9,10 @@ async function getData() {
     //  revalidate: 20, ///< ISR revalidate
     //},
   });
+  if (!res.ok) {
+    // Обработка ошибочных или неожиданных ответов
+    throw new Error('API response error');
+  }
   const hello = await res.json();
   return hello;
 }
@@ -20,65 +24,8 @@ export default async function Home() {
 
   return (
     <main className={styles.main}>
-      <div className={styles.description}>
-        <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>src/app/page.tsx</code>
-        </p>
-        
-      </div>
-
-
       <div className={styles.grid}>
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Docs <span>-&gt;</span>
-          </h2>
-          <p>Find in-depth information about Next.js features and API.</p>
-        </a>
         {data && data.map((item) => <div style={{color: 'tomato', fontSize: '40px'}} key={1}>{item.hello}</div>)}
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Learn <span>-&gt;</span>
-          </h2>
-          <p>Learn about Next.js in an interactive course with&nbsp;quizzes!</p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Templates <span>-&gt;</span>
-          </h2>
-          <p>Explore starter templates for Next.js.</p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Deploy <span>-&gt;</span>
-          </h2>
-          <p>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
       </div>
     </main>
   );
